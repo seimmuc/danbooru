d68293c447459ef08cd95c8941d298bae3de35bb
seimmuc <seimmuc@users.noreply.github.com>
2017-09-01 13:08:59 -0700

    trying something

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :user, "danbooru"
 set :rails_env, "production"
 server "DanbooruProduction", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
-set :linked_files, fetch(:linked_files, []).push(".env.production")
+#set :linked_files, fetch(:linked_files, []).push(".env.production")
