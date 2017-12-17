d029b4e7783356ae9397974a68a56ecb9fa7cf98
seimmuc <29907321+seimmuc@users.noreply.github.com>
2017-12-17 00:17:17 -0800

    new hostname

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "DanbooruProduction", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "railsproduction", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
-#set :linked_files, fetch(:linked_files, []).push(".env.production")
+set :linked_files, fetch(:linked_files, []).push(".env.production")
